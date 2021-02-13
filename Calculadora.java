72142391ddeb3278cc8ebd3ca31710d38ead5401
Tobiny <fernandochajim@gmail.com>
2021-02-13 16:53:50 -0600

    Version beta
    Se completo la versión beta, falta revisar que no haya problemas

--- Calculadora.java
+++ Calculadora.java
@@ -3,7 +3,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.*;
 import javax.swing.plaf.LayerUI;
-
+import java.math.*;
 
 public class Calculadora implements ActionListener {
 
@@ -12,7 +12,7 @@ public class Calculadora implements ActionListener {
     private JTextField resTxtF, opeTxtF;
     private JButton numButton[], opeButton[], pButton;
     private double res;
-    
+
     public Calculadora(){
         frame = new JFrame("Calculadora");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -98,85 +98,87 @@ public class Calculadora implements ActionListener {
     private String t = "";
     private int c = 0;
     private int ope = 0;
-
+    private int resuelto = 0;
+    private int contPunto[] = {0, 0};
     public void actionPerformed(ActionEvent e) {
         JButton s = (JButton) e.getSource();
         
         //Evaluando cada botón
-        if(s == numButton[0]){
-            //0
-            n[c] = n[c] + 0;
-            t = t + '0';
-        }
-        if(s == numButton[1]){
-            //1
-            n[c] = n[c] + 1;
-            t = t.concat("1");
-        }
-        if(s == numButton[2]){
-            //2
-            n[c] = n[c] + 2;
-            t = t + '2';
-        }
-        if(s == numButton[3]){
-            //3
-            n[c] = n[c] + 3;
-            t = t + '3';
-        }
-        if(s == numButton[4]){
-            //4
-            n[c] = n[c] + 4;
-            t = t + 4;
-        }
-        if(s == numButton[5]){
-            //5
-            n[c] = n[c] + 5;
-            t = t + 5;
-        }
-        if(s == numButton[6]){
-            //6
-            n[c] = n[c] + 6;
-            t = t + 6;
-        }
-        if(s == numButton[7]){
-            //7
-            n[c] = n[c] + 7;
-            t = t + 7;
-        }
-        if(s == numButton[8]){
-            //8
-            n[c] = n[c] + 8;
-            t = t + 8;
-        }
-        if(s == numButton[9]){
-            //9
-            n[c] = n[c] + 9;
-            t = t + 9;
+        if (resuelto != 1) {
+            if(s == numButton[0]){
+                //0
+                n[c] = n[c] + 0;
+                t = t + '0';
+            }
+            if(s == numButton[1]){
+                //1
+                n[c] = n[c] + 1;
+                t = t.concat("1");
+            }
+            if(s == numButton[2]){
+                //2
+                n[c] = n[c] + 2;
+                t = t + '2';
+            }
+            if(s == numButton[3]){
+                //3
+                n[c] = n[c] + 3;
+                t = t + '3';
+            }
+            if(s == numButton[4]){
+                //4
+                n[c] = n[c] + 4;
+                t = t + 4;
+            }
+            if(s == numButton[5]){
+                //5
+                n[c] = n[c] + 5;
+                t = t + 5;
+            }
+            if(s == numButton[6]){
+                //6
+                n[c] = n[c] + 6;
+                t = t + 6;
+            }
+            if(s == numButton[7]){
+                //7
+                n[c] = n[c] + 7;
+                t = t + 7;
+            }
+            if(s == numButton[8]){
+                //8
+                n[c] = n[c] + 8;
+                t = t + 8;
+            }
+            if(s == numButton[9]){
+                //9
+                n[c] = n[c] + 9;
+                t = t + 9;
+            }
         }
         if(s == opeButton[0]){
             //=
             efectuaOp(n, ope);
+            resuelto = 1;
         }
         if(s == opeButton[1]){
             //+
-            ope = 1;
+            
             if(valida(t, c)){
                 t = t + '+';
                 c++;
+                ope = 1;
             }
-
-            
-
         }
         if(s == opeButton[2]){
             //-
-            
-            ope = 2;
+
             if(t.isEmpty()){
                 t = t + '-';
             } else if(valida(t, c)){
                 t = t + '-';
                 c++;
+                ope = 2;
             }
 
         }
@@ -199,16 +201,28 @@ public class Calculadora implements ActionListener {
         }
         if(s == opeButton[5]){
             //π
-            t = t + 3.1416;
+            n[c] = "3.14159265359";
+            if (c==1) {
+                t = n[0] + operando() + n[1];
+            } else {
+                t = n[0];
+            }
+            
         }
         if(s == opeButton[6]){
             //√
-            ope = 5;
-            if(valida(t, c)){
-                t = t + '√';
-                c++;
+            
+            if(valida(t, c) && ope != 5){
+                if(c!=1){
+                    t = '√' + t;
+                    ope = 5;
+                    efectuaOp(n, ope);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Error, la raíz es de un solo número", "Error - Raíz", JOptionPane.WARNING_MESSAGE);
+                }
+                
             }
-
+            
         }
         if(s == opeButton[7]){
             //C
@@ -218,6 +232,9 @@ public class Calculadora implements ActionListener {
             n[0] = "";
             n[1] = "";
             resTxtF.setText("");
+            resuelto = 0;
+            contPunto[0] = 0;
+            contPunto[1] = 0;
         }
         if(s == opeButton[8]){
             //^
@@ -228,11 +245,12 @@ public class Calculadora implements ActionListener {
             //.
 
             if(!t.isEmpty()){
-                if(!(t.charAt(t.length()-1) == '-' ||  t.charAt(t.length()-1) == '+' || t.charAt(t.length()-1) == '*' || t.charAt(t.length()-1) == '/' || t.charAt(t.length()-1) == '-' || t.charAt(t.length()-1) == '^')){
+                if((!(t.charAt(t.length()-1) == '-' ||  t.charAt(t.length()-1) == '+' || t.charAt(t.length()-1) == '*' || t.charAt(t.length()-1) == '/' || t.charAt(t.length()-1) == '-' || t.charAt(t.length()-1) == '^' ||  t.charAt(t.length()-1) == '√')) && contPunto[c]!=1){
                     n[c] = n[c] + '.';
                     t = t + '.';
+                    contPunto[c] = 1;
                 } else {
-                    JOptionPane.showMessageDialog(null, "Error", "Titulo ventana", JOptionPane.WARNING_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Hubo un error en su escritura del punto", "Error - Punto", JOptionPane.WARNING_MESSAGE);
                 }
             }
             
@@ -246,31 +264,57 @@ public class Calculadora implements ActionListener {
     private void efectuaOp(String[] n, int ope){
         double a, b;
         a = Double.parseDouble(n[0]);
-        b = Double.parseDouble(n[1]);
+        b = 0;
+        if(ope != 5) b = Double.parseDouble(n[1]);
         switch (ope) {
             case 1:
                 resTxtF.setText(Double.toString(a+b));
                 
                 break;
             case 2:
-                
+                resTxtF.setText(Double.toString(a-b));
                 break;
             case 3:
-                
+                resTxtF.setText(Double.toString(a*b));
                 break;
             case 4:
-                
+                resTxtF.setText(Double.toString(a/b));
                 break;
             case 5:
-                
+                resTxtF.setText(Double.toString(Math.sqrt(a)));
                 break;
             case 6:
-                
+                resTxtF.setText(Double.toString(Math.pow(a,b)));
                 break;
         
             default:
                 break;
         }
+    }
+    private char operando(){
+        char opera ='-';
+        switch (ope) {
+            case 1:
+                opera = '+';
+
+                break;
+            case 2:
+                opera = '-';
+                break;
+            case 3:
+                opera = '*';
+                break;
+            case 4:
+                opera = '/';
+                break;
+            case 6:
+                opera = 'r';
+                break;
+        
+            default:
+                break;
+        }
+        return opera;
     }
     private boolean valida(String a, int l){
         boolean c = false;
@@ -278,9 +322,9 @@ public class Calculadora implements ActionListener {
             if((!(a.charAt(a.length()-1) == '-' ||  a.charAt(a.length()-1) == '+' || a.charAt(a.length()-1) == '*' || a.charAt(a.length()-1) == '/' || a.charAt(a.length()-1) == '-' || a.charAt(a.length()-1) == '^')) && l != 1){
                 c = true;
             } else {
-                JOptionPane.showMessageDialog(null, "Error", "Titulo ventana", JOptionPane.WARNING_MESSAGE);
+                JOptionPane.showMessageDialog(null, "Hubo algún error en su escritura", "Error - Escritura", JOptionPane.WARNING_MESSAGE);
             }
-        } else JOptionPane.showMessageDialog(null, "Error", "Titulo ventana", JOptionPane.WARNING_MESSAGE);
+        } else JOptionPane.showMessageDialog(null, "Hubo algún error en su escritura", "Error - Escritura", JOptionPane.WARNING_MESSAGE);
         return c;
     }
 
